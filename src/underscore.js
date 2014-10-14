d88793b132e276aefc7bb0b5165320d5e1ca43e9
MWMCKENNA <MWMCKENNA@GMAIL.COM>
2014-10-14 09:23:02 -0700

    initial commit

--- src/underscore.js
+++ src/underscore.js
@@ -3,25 +3,41 @@ var myFunctions = {
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   first: function(array, n) {
-
-  },
+    if (n > 1) {
+      for (var i = n; i < array.length; i--) {
+      return array.indexOf[i];
+     }
+    }
+    else {
+      return array.indexOf[1];
+    }
+   },
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   last: function(array, n) {
-
+    if (n < array.length) {
+      return array.indexOf[n];
+      (n--);
+    } else {
+      return array.indexOf[n];
+    }
   },
 
   // Produce a duplicate-free version of the array.
   uniq: function(array) {
-
+    var a = [];
+    for (var i=0, l=array.length; i<l; i++)
+        if (a.indexOf(array[i]) === -1 && array[i] !== '')
+            a.push(array[i]);
+    return a;
   },
 
   // Takes an array of objects and returns and array of the values of
   // a certain property in it. E.g. take an array of people objects
   // (which have a name and an age) and return an array of just their ages
   pluck: function(array, key) {
-
+    return map(array, property(key));
   },
 
  // Determine if the array contains a given value (using `===`).
